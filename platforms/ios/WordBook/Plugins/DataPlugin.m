9cc2892079a47ce7135c30efe1ecd7377255a96a
momirror <471488376@qq.com>
2016-03-05 23:42:45 +0800

    update css for wordDetail page

--- platforms/ios/WordBook/Plugins/DataPlugin.m
+++ platforms/ios/WordBook/Plugins/DataPlugin.m
@@ -21,12 +21,10 @@
     NSArray * translate = basic[@"explains"];
     Word * word = [[Word alloc] init];
     NSLog(@"%@",[translate description] );
-    word.translate = [[translate description] dataUsingEncoding:NSUTF8StringEncoding];
+    word.translate = [translate description];
     word.usphonetic = basic[@"us-phonetic"];
     word.ukphonetic = basic[@"uk-phonetic"];
     word.word = dic[@"query"];
-    
-    NSLog(@"%@",[[NSString alloc] initWithData:word.translate encoding:NSUTF8StringEncoding]);
     
     [[DataManagement instance] addIntoDataSource:word];
     
@@ -34,18 +32,12 @@
 
 - (void)getAllData:(CDVInvokedUrlCommand *)command
 {
-    NSLog(@"getAllData");
     
     CDVPluginResult* result = nil;
-
-    NSDictionary * dicData = @{@"word":@"China",@"translate":@"中国"};
     
     NSArray * datas = [[DataManagement instance] queryAllData];
-//    NSArray * datas = @[@"1",@"2",@"3"];
+
     result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsArray:datas];
-    
-//    result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:dicData];
-    NSLog(@"%@",result.status);
     
     [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 }
